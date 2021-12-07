e85e61e46224b6cad1ef37abbef4c4b0cc6f7242
4LTon <81396749+4LTon@users.noreply.github.com>
2021-12-07 08:59:36 +0300

    the answer to the 15th task has been added

---
+++ Week_7/Task-15/BlackListFilter.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface BlackListFilter<T> {
+
+    /**
+     * From the given list of comments removes ones
+     * that contain words from the black list.
+     *
+     * @param comments list of comments; every comment
+     *                 is a sequence of words, separated
+     *                 by spaces, punctuation or line breaks
+     * @param blackList list of words that should not
+     *                  be present in a comment
+     */
+
+    default List<T> filterComments(Iterable<T> comments, Predicate<T> blackList) {
+        ArrayList<T> filtrationResult = new ArrayList<>();
+        for (T comment : comments) {
+            if (!blackList.test(comment)) filtrationResult.add(comment);
+        }
+
+        return filtrationResult;
+    }
+}
