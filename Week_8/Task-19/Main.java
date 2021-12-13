c1abb211a13d8697766fd04dfe2df2e6c2c8bf83
4LTon <81396749+4LTon@users.noreply.github.com>
2021-12-13 14:08:48 +0300

    improving the Main file

--- Week_8/Task-19/Main.java
+++ Week_8/Task-19/Main.java
@@ -2,9 +2,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        
         try (FileWriter writer = new FileWriter("example.txt", false)) {
 
             writer.write("Hello, World!");
