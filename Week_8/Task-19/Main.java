10aae2dbe0f056580fb9ce7bd70ac5f5d9d834cd
4LTon <81396749+4LTon@users.noreply.github.com>
2021-12-13 14:04:22 +0300

    the answer to the 19th task has been added

---
+++ Week_8/Task-19/Main.java
@@ -0,0 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        try (FileWriter writer = new FileWriter("example.txt", false)) {
+
+            writer.write("Hello, World!");
+            writer.flush();
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
